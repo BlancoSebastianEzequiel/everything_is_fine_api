bb3b405bbd4d51f6979b66c14c940cfe0c9ee615
Gonzalo Bordanzi <gbordanzi@devartis.com>
2020-02-13 16:54:52 -0300

    added base api

---
+++ src/api.rb
@@ -0,0 +1,22 @@
+require 'sinatra'
+require 'sinatra/json'
+require 'json'
+
+set :bind, '0.0.0.0'
+set :port, 3002
+
+FILE_BASE='/home/gbordanzi/trabajo/keepcon/everythin_is_fine_api'
+
+get '/frank-says' do
+	json(foo: 'Put this in your pipe & smoke it!')
+end
+
+post '/event_flow_test' do
+	body = JSON.parse(request.body.read)
+
+	open("#{FILE_BASE}/event_flow_test", 'a') do |file|
+		file.puts(body.to_json)
+		file.puts("\n")
+	end
+	json(status: 'received', detail: 'all green')
+end
