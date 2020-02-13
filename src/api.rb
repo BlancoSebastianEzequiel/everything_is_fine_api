d379116cbcf9bfebc75cd83a9d49e438eafb25ec
Gonzalo Bordanzi <gbordanzi@devartis.com>
2020-02-13 17:26:33 -0300

    Added class to support ssl

--- src/api.rb
+++ src/api.rb
@@ -1,8 +1,11 @@
 require 'sinatra'
 require 'sinatra/json'
+require_relative 'sinatra_ssl'
 require 'json'
 
 set :bind, '0.0.0.0'
+set :ssl_certificate, 'localhost.crt'
+set :ssl_key, 'localhost.key'
 set :port, 3002
 
 FILE_BASE='/home/gbordanzi/trabajo/keepcon/everythin_is_fine_api'
