6775ef40d2ea8c605fe098f884f2a87bb9eb3a5d
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2020-05-11 13:38:13 -0300

    Add dummy endpoint for facebook graph appi mock

--- src/api.rb
+++ src/api.rb
@@ -1,19 +1,31 @@
 require 'sinatra'
-require 'sinatra/json'
 require 'json'
 
-require_relative 'sinatra_ssl'
-require_relative 'sinatra_auth'
+# require_relative 'sinatra_ssl'
+# require_relative 'sinatra_auth'
 
 set :bind, '0.0.0.0'
-set :ssl_certificate, 'localhost.crt'
-set :ssl_key, 'localhost.key'
-set :port, 3002
+# set :ssl_certificate, 'localhost.crt'
+# set :ssl_key, 'localhost.key'
+set :port, 3005
 
-FILE_BASE='/home/gbordanzi/trabajo/keepcon/everythin_is_fine_api'
+FILE_BASE='/home/sebastian/Projects/everything_is_fine_api'
+
+before do
+	content_type :json
+end
+
 
 get '/frank-says' do
 	json(foo: 'Put this in your pipe & smoke it!')
+end
+
+get '/:id' do
+  fields = params[:fields]
+  access_token = params[:access_token]
+  return JSON.parse(File.read("#{FILE_BASE}/src/last_page.json")) if params[:next]
+
+	JSON.parse(File.read("#{FILE_BASE}/src/first_page.json"))
 end
 
 post '/event_flow_test' do
@@ -35,4 +47,10 @@ post '/event_flow_test' do
 		end
 		json(status: 'todo mal ameo', detail: 'en algun lado la cagaste')
 	end
+end
+
+
+after do
+	response.body = JSON.dump(response.body)
+	response.headers['x-app-usage'] = "{\"call_count\":4,\"total_cputime\":0,\"total_time\":1}"
 end
